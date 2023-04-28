bd23ce4027715944e21f9c1fd3fe4fd83fc9ca0b
huangqj <chris_junjun@163.com>
2023-04-28 17:27:55 +0800

    增加全局分页组件

---
+++ src/components/Pagination.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="table-pagination">
+    <el-pagination
+      small
+      background
+      :layout="layout"
+      :page-sizes="pageSizes"
+      :total="page.total"
+      :page-size="page.pageSize"
+      :current-page="page.pageNumber"
+      @size-change="handleSizeChange"
+      @current-change="handleNumberChange"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Pagination',
+  data() {
+    return {};
+  },
+  props: {
+    layout: {
+      default: () => {
+        return 'total, sizes, prev, pager, next, jumper';
+      }
+    },
+    pageSizes: {
+      default: () => {
+        return [10, 30, 50];
+      }
+    },
+    page: {
+      default: () => {
+        return {
+          total: 0,
+          pageSize: 10,
+          pageNumber: 1
+        };
+      }
+    }
+  },
+  mounted() {
+    console.log(this.layout);
+    console.log(this.pageSizes);
+    console.log(this.page);
+  },
+  methods: {
+    handleSizeChange(val) {
+      this.page.pageSize = val;
+      this.$emit('initData');
+    },
+    handleNumberChange(val) {
+      this.page.pageNumber = val;
+      this.$emit('initData');
+    }
+  }
+};
+</script>
